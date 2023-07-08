da42b801281aca3c0704f9fc5372d244e79ae2a4
Cortes-Ces <Cortesces90@gmail.com>
2023-07-08 07:24:59 -0400

    First attempt at RPS game

---
+++ script.js
@@ -0,0 +1,50 @@
+// Set array to store the string choices
+const choices = ["rock", "paper", "scissors"];
+// Randomize computer choice
+function getComputerChoice() {
+    // Use random function to randomize choices based of array size
+    const random = Math.floor(Math.random() * choices.length);
+    //console.log(choices[random]);
+    return choices[random];
+}
+
+// Display results within a single round
+function singleRound(playerSelection, computerSelection) {
+    // Will make the input strings into case insensitive
+    computerSelection = getComputerChoice().toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+
+    if (computerSelection == playerSelection) {
+        console.log("Tie game!");
+      } else if (
+        (computerSelection == "rock" && playerSelection == "scissors") ||
+        (computerSelection == "scissors" && playerSelection == "paper") ||
+        (computerSelection == "paper" && playerSelection == "rock")) {
+            console.log("The computer wins");
+        } else if (
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")) {
+            console.log("The player wins")
+        }
+}
+
+// Comp and player choices
+const player = "rock";
+const computer = getComputerChoice();
+
+// Works but will fix later using for loop
+ function game() {
+    // If round is less than 5 repeat NOT DONE YET
+    for (let i = 1; i < 7; i++) { 
+        Math.random(singleRound(player, computer));
+        console.log("Round: " + i)
+      }
+      
+    }; 
+//console.log(game());
+game();
+// game();
+// game();
+// game();
+// game();
